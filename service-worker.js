100dd31ec1e937ec06828a4d6f28c03f6454ce88
heitorperocini-ux <heitorperocini@gmail.com>
2025-11-06 13:07:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='contagem-pasteis-v1';
+const CACHE_NAME='contagem-pasteis-v2';
 const ASSETS=["./", "./index.html", "./manifest.json", "./contagem-pasteis-icon-512.png", "./contagem-pasteis-icon-256.png", "./contagem-pasteis-icon-192.png", "./contagem-pasteis-icon-180.png", "./contagem-pasteis-icon-152.png", "./contagem-pasteis-icon-128.png", "./contagem-pasteis-icon-96.png", "./contagem-pasteis-icon-72.png", "./contagem-pasteis-icon-48.png"];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
